bc40b9f166fe444fd5228e609d969b207e1806be
om.devwork <om.devwork@gmail.com>
2024-09-14 20:18:52 +0530

    created sidebar component

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -1,15 +1,19 @@
 "use client"
 
+import { Sidebar } from "./sidebar";
 import { ToolBar } from "./toolbar";
 
 interface WorkspaceIdLayoutProps {
     children: React.ReactNode;
 }
 
-const WorkspaceIdLayout = ({children}: WorkspaceIdLayoutProps) => {
+const WorkspaceIdLayout = ({ children }: WorkspaceIdLayoutProps) => {
     return <div className="h-full">
         <ToolBar />
-        {children}
+        <div className="flex h-[calc(100vh-40px)]">
+            <Sidebar />
+            {children}
+        </div>
     </div>
 }
 
--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,15 +1,9 @@
-"use client"
-
-import { useWorkSpaceId } from '@/hooks/use-workspace-id';
-import { useGetWorkspace } from '@/features/workspaces/api/use-get-workspace';
 
 const WorkspaceIdPage = () => {
 
-    const workspaceId = useWorkSpaceId();
-    const {data} = useGetWorkspace({id: workspaceId});
     return (
         <div>
-            Data: {JSON.stringify(data)}
+            Workspace id page
         </div>
     )
 }
