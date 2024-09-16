90708c2761573a5baa588f4b57b07db6759e2f0e
om.devwork <om.devwork@gmail.com>
2024-09-16 16:46:55 +0530

    created workspace sidebar

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -1,7 +1,14 @@
 "use client"
 
+import {
+    ResizableHandle,
+    ResizablePanel,
+    ResizablePanelGroup,
+} from "@/components/ui/resizable"
+
 import { Sidebar } from "./sidebar";
 import { ToolBar } from "./toolbar";
+import { WorkspaceSidebar } from "./workspace-sidebar";
 
 interface WorkspaceIdLayoutProps {
     children: React.ReactNode;
@@ -12,7 +19,24 @@ const WorkspaceIdLayout = ({ children }: WorkspaceIdLayoutProps) => {
         <ToolBar />
         <div className="flex h-[calc(100vh-40px)]">
             <Sidebar />
-            {children}
+            <ResizablePanelGroup 
+            direction="horizontal"
+            autoSaveId="ca-workspace-layout"
+            >
+                <ResizablePanel
+                    defaultSize={20}
+                    minSize={11}
+                    className="bg-[#5E2C5F]"
+                >
+                    <WorkspaceSidebar />
+                </ResizablePanel>
+                <ResizableHandle withHandle />
+                <ResizablePanel
+                    minSize={20}
+                >
+                    {children}
+                </ResizablePanel>
+            </ResizablePanelGroup>
         </div>
     </div>
 }
