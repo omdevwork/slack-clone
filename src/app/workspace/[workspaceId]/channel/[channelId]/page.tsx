7bb8b2c2f0e594499bac39e704508b3107a679c5
om.devwork <om.devwork@gmail.com>
2024-10-17 15:11:47 +0530

    added editor component

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -7,6 +7,7 @@ import { useGetChannel } from "@/features/channels/api/use-get-channel";
 import { useChannelId } from "@/hooks/use-channel-id";
 
 import { Header } from "./header";
+import { ChatInput } from "./chat-input";
 
 const ChannelIdPage = () => {
 
@@ -36,6 +37,8 @@ const ChannelIdPage = () => {
     return (
         <div className="flex flex-col h-full">
             <Header title={channel.name} />
+            <div className="flex-1" />
+            <ChatInput placeholder={`Message #${channel.name}`} />
         </div>
     );
 }
