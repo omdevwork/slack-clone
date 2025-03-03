61ccd935afe82538edd1ee80bace05b99e4318ac
omdevwork <om.devwork@gmail.com>
2025-03-03 18:12:23 +0530

    Messages list component

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -6,6 +6,7 @@ import { useGetChannel } from "@/features/channels/api/use-get-channel";
 import { useGetMessages } from "@/features/messages/api/use-get-message";
 
 import { useChannelId } from "@/hooks/use-channel-id";
+import { MessageList } from "@/components/message-list";
 
 import { Header } from "./header";
 import { ChatInput } from "./chat-input";
@@ -14,11 +15,10 @@ const ChannelIdPage = () => {
 
     const channelId = useChannelId();
 
-    const { results } = useGetMessages({ channelId })
+    const { results, status, loadMore } = useGetMessages({ channelId })
     const { data: channel, isLoading: channelLoading } = useGetChannel({ id: channelId })
 
-    console.log("🚀 ~ ChannelIdPage ~ results:", results)
-    if (channelLoading) {
+    if (channelLoading || status === "LoadingFirstPage") {
         return (
             <div className="h-full flex-1 flex items-center justify-center">
                 <Loader className="size-5 animate-spin text-muted-foreground" />
@@ -40,9 +40,14 @@ const ChannelIdPage = () => {
     return (
         <div className="flex flex-col h-full">
             <Header title={channel.name} />
-            <div className="flex-1">
-                {JSON.stringify(results)}
-            </div>
+            <MessageList
+                channelName={channel.name}
+                channelCreationTime={channel._creationTime}
+                data={results}
+                loadMore={loadMore}
+                isLoadingMore={status === "LoadingMore"}
+                canLoadMore={status === "CanLoadMore"}
+            />
             <ChatInput placeholder={`Message #${channel.name}`} />
         </div>
     );
